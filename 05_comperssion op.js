aeafc58f80e71fc0c984e703442f2912e372b2d5
Abhishek Shah <128625393+abhishekpro29@users.noreply.github.com>
2024-01-12 11:28:50 +0530

    Add files via upload

---
+++ 05_comperssion op.js
@@ -0,0 +1,20 @@
+
+/*console.log(2<1);
+console.log(2>1);
+console.log(2<=1);
+console.log(2>=1);
+console.log(2!=1);
+console.log(2==1);
+
+console.log("2"<1);// not use to prefer 
+differnt data taype operation
+console.log("2"<=1);
+console.log("2">=1);
+console.log("2"!=1);
+console.log("2"==1); 
+<--!comerisson and equl are diff in js -->
+*/
+
+console.log(""!=1);
+console.log(null!=1);
+console.log("2"===2);
