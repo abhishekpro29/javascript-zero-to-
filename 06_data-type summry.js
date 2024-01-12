970df39b51254d52ec38ea2907dbfb787ed98ebe
Abhishek Shah <128625393+abhishekpro29@users.noreply.github.com>
2024-01-12 11:27:55 +0530

    Add files via upload

---
+++ 06_data-type summry.js
@@ -0,0 +1,23 @@
+//premitive data
+// 7type :str,number,boolean,null,undefine,symbol,bigint
+//refernce non-premitive
+
+const id = Symbol('123')
+const user_id = Symbol('123')
+console.log(id===user_id);
+
+//constBigInt =2222222222222
+//const Tem_State=null
+//console.log(Tem_State);
+
+
+//array,objects,function
+//const hero= ["thor","iron-man","cpation-ac"];
+//console.log(hero);
+//const hero_obj={ //  name:"thor",movies:"evengers",
+
+//}
+const hero_function=function()
+{
+    console.log("hello world");
+}
---
+++ 08_string.js
@@ -0,0 +1,6 @@
+const Shirt_color="blue"  // object string che
+const Price =500
+
+//console.log(Shirt_color+Price);
+console.log("shirt color  is ${Shirt_color} and price is ${Price}");
+
